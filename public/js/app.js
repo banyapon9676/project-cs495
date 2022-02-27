f5283374ecd26fa30a867853b484e3ad3875aca1
banyapon9676 <banyapon9676@gmail.com>
2022-02-27 21:11:38 +0700

    fond commit

--- public/js/app.js
+++ public/js/app.js
@@ -29430,7 +29430,7 @@ var staticRenderFns = [
       _c("img", {
         staticClass: "img-fluid",
         attrs: {
-          src: "https://scontent.fbkk12-4.fna.fbcdn.net/v/t39.30808-6/274495483_3573770756182649_5682521323026761252_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeGfvjyix2CsCMWpduNzyfoutBx2A5lg2LC0HHYDmWDYsHGhx-LYDlie24FD7RCG2H-BLeqOV-wxhSK_6UYXEoS5&_nc_ohc=pi-SiLZy3GcAX8VeEO0&_nc_ht=scontent.fbkk12-4.fna&oh=00_AT-ik5hjjB9uJLrbp_spGVXIh4HqsO7Nl36iCLewju3BMw&oe=621950C8",
+          src: "https://scontent.fcnx3-1.fna.fbcdn.net/v/t39.30808-6/271871178_3542730269286698_3375559899961798443_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeH3MdfsConwbZNli8awwO7DPcEBuVeAD_M9wQG5V4AP8yIv2qf70WYJ7mbWBbT7xA3PnXmunkhVdmy4qP-tGcpb&_nc_ohc=mynp1tt3ma8AX_P8e79&tn=u5Sb0-YIOHJ9kqR9&_nc_ht=scontent.fcnx3-1.fna&oh=00_AT9C8T6Phtj0gOe-KfARQG8jrvgbBnQs00QUXV7_oZlk4Q&oe=62214EDE",
           alt: "...",
         },
       }),
--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -59,7 +59,7 @@
       aria-hidden="true"
     >
       <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content"> 
           <div class="modal-header">
             <h5 class="modal-title" id="readLabel">{{ furName }}</h5>
             <button
